02b5d698c693a9e1022f700cfbe62537caa7584e
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-25 14:09:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 import os
 
-BOT_TOKEN = os.getenv("7623579455:AAHl_qRDh3Qcz9YRBhPRR7aXasIheVVYtzw")
+# Вставь свой токен Telegram-бота
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "7623579455:AAHl_qRDh3Qcz9YRBhPRR7aXasIheVVYtzw"
+
+# Значения сумм, которые будут в кнопках
+AVAILABLE_AMOUNTS = [10000, 20000, 50000, 100000]
+
+# Минимальный спред, с которого начинается уведомление
+MIN_PROFIT_PERCENT = 3.0
+
+# Интервал проверки (в секундах)
+CHECK_INTERVAL = 60
