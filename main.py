b8657ad72aad5f3eeb4b1efdcdcc20bf429b19cb
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-25 14:38:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+import asyncio
 from config import BOT_TOKEN
-from handlers.menu import router
-from arbitrage import start_arbitrage_monitoring
 from keep_alive import keep_alive
+from handlers import menu
+
+from arbitrage import start_arbitrage_monitoring
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
-    dp.include_router(router)
+    dp.include_router(menu.router)
 
-    keep_alive()
+    # Запуск автоарбитража
     asyncio.create_task(start_arbitrage_monitoring(bot))
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    keep_alive()
     asyncio.run(main())
