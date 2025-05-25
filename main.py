04bc74b66ae05d1ca8cb684b7af1dd406bdb2633
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-25 12:09:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import asyncio
-from bot import run_bot
+from bot import dp, bot
+from keep_alive import keep_alive
+
+async def run_bot():
+    keep_alive()
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(run_bot())
