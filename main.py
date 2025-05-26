b35956b75712924a850382a341ce547aa17cfe4a
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-26 05:48:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from bot import router
-from config import BOT_TOKEN
+from aiogram import Dispatcher
+from bot import dp
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(bot)
-    dp.include_router(router)
-    await dp.start_polling()  # без параметров
+    print("Бот запущен!")
+    await dp.start_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
