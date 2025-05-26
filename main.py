34f41f0fead1253a55803cbd1b9740372cf81596
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-26 06:01:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import asyncio
-from bot import dp
+from bot import dp, bot
 
 async def main():
-    await dp.start_polling()
+    print("Бот запускается...")
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
