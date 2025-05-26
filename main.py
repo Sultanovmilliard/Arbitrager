ea867458d7a10f414c3d0ecdabc5e9a615808cbb
Sultanovmilliard <nail.sultanov777@icloud.com>
2025-05-26 05:18:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(bot)
     dp.include_router(router)
-    await dp.start_polling(bot)
+    await dp.start_polling()  # без параметров
 
 if __name__ == "__main__":
     asyncio.run(main())
